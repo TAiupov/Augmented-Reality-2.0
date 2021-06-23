865d555f6ba63c529c4ac3cb05f420a8520d3d60
Tatar <aiupovtagir@gmail.com>
2021-06-23 09:53:12 -0700

    Initial Commit

---
+++ ARF/ARFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFApp.swift
+//  ARF
+//
+//  Created by Тагир Аюпов on 2021-06-23.
+//
+
+import SwiftUI
+
+@main
+struct ARFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARF/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARF
+//
+//  Created by Тагир Аюпов on 2021-06-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
